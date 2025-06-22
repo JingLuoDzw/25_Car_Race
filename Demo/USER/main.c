5a1385b7266d1ed7e92d8b5cc10fb828b8d3ce24
JingLuo <2109206112@qq.com>
2025-06-22 21:38:05 +0800

    150实测一米三稳定，180实测一米五较稳定，200不太行，期末前封车

--- Demo/USER/main.c
+++ Demo/USER/main.c
@@ -7,18 +7,55 @@ extern float Sum_Distance;
 //char state[30] = {Track, Track, Track, Track, Track, Track, Stop};	
 char state[30] = {Track, Small_Circ_Left, Track, Small_Circ_Left, Track, Small_Circ_Left, Track, Small_Circ_Left, Track, Track, Track, Stop};	//2025赛道元素顺序
 
-//速度
+////速度200
+//int SPEED = 200 ;
+////转向环
+//int Kp1[5]=  { 350 , 0 , 0 , 0 , 0 };		//320	290	0.3除几字弯还好		300	320	0.4几字弯还可以，折线勉强
+//int Kp2[5]=  { 370 , 0 , 0 , 0 , 0 };
+//float Kd1[5]={ 0.63 , 0 , 0 , 0 , 0 };
+//float Kd2[5]={ 30 , 0 , 0 , 0 , 0 };
+////左环电感判断条件
+//int Circ_Jump_Left[4] = { 1900 , 1000 , 500 , 1900 };
+////左环状态判断条件
+//int Circ_Left_Dis_Start = 15 ;
+//int Circ_Left_Angle = 335 ;
+//int Circ_Left_Dis_Final = 50 ;
+////左环行为
+//int Circ_Left_Speed_L = 180 ;
+//int Circ_Left_Speed_R = 300 ;
+//int Circ_Left_Speed_Out = 180 ;
+////出库
+//float Out_Jump_Angle = - 70 ;
+//int Out_Speed_L = 280 ;
+//int Out_Speed_R = 80 ;
+////右环
+//int Circ_Jump_Right[4] = { 1200 , 0 , 200 , 1000 };
+//int Circ_Right_Dis_Start = 10 ;
+//int Circ_Right_Angle = - 340 ;
+//int Circ_Right_Dis_Final = 80 ;
+//int Circ_Right_Speed_L = 250 ;
+//int Circ_Right_Speed_R = 150 ;
+//int Circ_Right_Speed_Out = 150 ;
+////速度环PID	150
+//int KP_SPEED_L = 100 ;
+//int KP_SPEED_R = 100 ;
+//int KI_SPEED_L = 19 ;
+//int KI_SPEED_R = 19 ;
+//int KD_SPEED_L = 0 ;
+//int KD_SPEED_R = 0 ;
+
+//速度180
 int SPEED = 180 ;
 //转向环
-int Kp1[5]=  { 190 , 0 , 0 , 0 , 0 };		//320	290	0.3除几字弯还好
-int Kp2[5]=  { 250 , 0 , 0 , 0 , 0 };
-float Kd1[5]={ 0.25 , 0 , 0 , 0 , 0 };
-float Kd2[5]={ 0 , 0 , 0 , 0 , 0 };
+int Kp1[5]=  { 300 , 0 , 0 , 0 , 0 };		//320	290	0.3除几字弯还好		300	320	0.4几字弯还可以，折线勉强
+int Kp2[5]=  { 320 , 0 , 0 , 0 , 0 };
+float Kd1[5]={ 0.42 , 0 , 0 , 0 , 0 };
+float Kd2[5]={ 15 , 0 , 0 , 0 , 0 };
 //左环电感判断条件
 int Circ_Jump_Left[4] = { 1900 , 1000 , 500 , 1900 };
 //左环状态判断条件
 int Circ_Left_Dis_Start = 15 ;
-int Circ_Left_Angle = 340 ;
+int Circ_Left_Angle = 335 ;
 int Circ_Left_Dis_Final = 50 ;
 //左环行为
 int Circ_Left_Speed_L = 180 ;
@@ -114,8 +151,8 @@ int main()
             {
                 uart_delay = 0;
 							
-								printf("%d,%d,%d,%d\r\n", adc_value[0], adc_value[1],
-																					adc_value[3], adc_value[4]);
+//								printf("%d,%d,%d,%d\r\n", adc_value[0], adc_value[1],
+//																					adc_value[3], adc_value[4]);
 							
 //                printf("%f,%f,%f,%f\r\n", motor_L_pid.ActValue, motor_R_pid.ActValue,
 //																					motor_L_pid.SetValueTmp, motor_R_pid.SetValueTmp);
